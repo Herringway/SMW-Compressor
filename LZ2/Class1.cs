b2a9024b7cd256958e97a402e4f65af13c9a69b0
ElPenguino <elpenguino@gmail.com>
2011-07-19 20:45:46 -0300

    Initial commit

---
+++ LZ2/Class1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LZ2
+{
+    public class LZ2
+    {
+        public static byte[] Decompress(Stream data)
+        {
+            BinaryReader Data = new BinaryReader(data);
+            List<byte> outputbuffer = new List<byte>();
+            byte command;
+            byte commandlength;
+            short longcommandlength;
+            ushort tempaddress;
+            byte tempbyte;
+            byte tempbyte2;
+            Boolean completed = false;
+
+            while (!completed)
+            {
+                tempbyte = Data.ReadByte();
+                command = (byte)((tempbyte >> 5) & 0x7);
+                commandlength = (byte)(tempbyte & 0x1F);
+
+                switch (command)
+                {
+                    case 0: //Direct Copy
+                        for (int i = 0; i <= commandlength; i++)
+                            outputbuffer.Add(Data.ReadByte());
+                        break;
+                    case 1: //Byte Fill
+                        tempbyte = Data.ReadByte();
+                        for (int i = 0; i <= commandlength; i++)
+                            outputbuffer.Add(tempbyte);
+                        break;
+                    case 2: //Word Fill
+                        tempbyte = Data.ReadByte();
+                        tempbyte2 = Data.ReadByte();
+                        for (int i = 0; i <= commandlength; i++)
+                        {
+                            if (i % 2 == 1)
+                                outputbuffer.Add(tempbyte2);
+                            else
+                                outputbuffer.Add(tempbyte);
+                        }
+                        break;
+                    case 3: //Increasing Fill
+                        tempbyte = Data.ReadByte();
+                        for (int i = 0; i <= commandlength; i++)
+                            outputbuffer.Add(tempbyte++);
+                        break;
+                    case 4: //Repeat
+                        tempaddress = BEShorttoLE(Data.ReadUInt16());
+                        if (tempaddress > outputbuffer.Count)
+                            throw new Exception("Bad Address!");
+                        for (int i = 0; i <= commandlength; i++)
+                            outputbuffer.Add(outputbuffer[tempaddress++]);
+                        break;
+                    case 5: //Unused
+                        throw new Exception("Unused command byte detected (5)");
+                    case 6: //Unused
+                        throw new Exception("Unused command byte detected (6)");
+                    case 7: //Long Command
+                        command = (byte)((commandlength >> 2) & 0x7);
+                        longcommandlength = (short)(((commandlength & 3) << 8) + Data.ReadByte());
+
+                        switch (command)
+                        {
+                            case 0: //Direct Copy
+                                for (int i = 0; i <= longcommandlength; i++)
+                                    outputbuffer.Add(Data.ReadByte());
+                                break;
+                            case 1: //Byte Fill
+                                tempbyte = Data.ReadByte();
+                                for (int i = 0; i <= longcommandlength; i++)
+                                    outputbuffer.Add(tempbyte);
+                                break;
+                            case 2: //Word Fill
+                                tempbyte = Data.ReadByte();
+                                tempbyte2 = Data.ReadByte();
+                                for (int i = 0; i <= longcommandlength; i++)
+                                {
+                                    if (i % 2 == 1)
+                                        outputbuffer.Add(tempbyte2);
+                                    outputbuffer.Add(tempbyte);
+                                }
+                                break;
+                            case 3: //Increasing Fill
+                                tempbyte = Data.ReadByte();
+                                for (int i = 0; i <= longcommandlength; i++)
+                                    outputbuffer.Add(tempbyte++);
+                                break;
+                            case 4: //Repeat
+                                tempaddress = BEShorttoLE(Data.ReadUInt16());
+                                if (tempaddress > outputbuffer.Count)
+                                    throw new Exception("Bad Address!");
+                                for (int i = 0; i <= longcommandlength; i++)
+                                    outputbuffer.Add(outputbuffer[tempaddress++]);
+                                break;
+                            case 5: //Unused
+                                throw new Exception("Unused command byte detected (5)");
+                            case 6: //Unused
+                                throw new Exception("Unused command byte detected (6)");
+                            case 7: //End
+                                completed = true;
+                                break;
+
+                        }
+                        break;
+
+                }
+            }
+
+            return outputbuffer.ToArray();
+        }
+        private static ushort BEShorttoLE(ushort input)
+        {
+            return (ushort)((input>>8) + (input<<8));
+        }
+    }
+}
---
+++ LZ2Test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LZ2;
+using System.IO;
+
+namespace LZ2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: {0} Input Output Offset", System.AppDomain.CurrentDomain.FriendlyName);
+                return;
+            }
+            Stream file;
+            try
+            {
+                file = File.Open(args[0], FileMode.Open);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            file.Seek(long.Parse(args[2]), SeekOrigin.Begin);
+            byte[] output = LZ2.LZ2.Decompress(file);
+            file.Close();
+            if (File.Exists(args[1]))
+                File.Delete(args[1]);
+            try
+            {
+                BinaryWriter outputFile = new BinaryWriter(File.Open(args[1], FileMode.OpenOrCreate));
+                outputFile.Write(output);
+                outputFile.BaseStream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+    }
+}
